3550d1ec5d70546211c0aa88bbc820570279702f
Chris Terrazas <chris.terrazas@gmail.com>
2019-01-17 15:53:19 -0500

    Deployment

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,7 @@ Rails.application.routes.draw do
 
   get 'user', to: 'user_info#index'
   get 'results', to: 'search_results#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  if Rails.env.production?
-    CLIENT_HTML = File.read(Rails.root.join('public/index.html'))
-
-    get "*path", to: proc { [200, {}, [CLIENT_HTML]] }
-  end
+ 
 end
 
 
